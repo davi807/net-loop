db6c5bc7fb7383ea54c480bca0f41b34868945e6
davi807 <davi807plus@yandex.ru>
2019-08-18 19:43:51 +0400

    ui items

---
+++ public/translations/en.js
@@ -0,0 +1,13 @@
+const mui = {
+    _name: 'en',
+    title: 'Network utils',
+
+    // Tools names
+    ping: 'Ping',
+    tcp: 'Tcp port check',
+
+    // Database
+    add: 'add',
+    remove: 'remove',
+    edit: 'edit',
+};
